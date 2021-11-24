d484441b53555042fbb60b7f751ff54f5e5b5d35
MayuriPatil30 <89208355+MayuriPatil30@users.noreply.github.com>
2021-11-24 14:29:37 +0530

    New sentence added and found new frequency

---
+++ HashMap.java
@@ -0,0 +1,74 @@
+public class HashMap
+{
+
+    static void remove(String str, String removeWord) 
+{
+        String msg[] = str.split(" ");
+        String new_str = " ";
+        for (String words : msg) 
+	 {
+            if (!words.equals(removeWord)) 
+ 	      {
+                new_str += words + " ";
+              }
+         }
+        System.out.println();
+        System.out.println("Removed the word -> avoidable");
+        System.out.println(new_str);
+}
+
+
+    public static void main(String[] args) 
+    {
+
+        String sentence = "To be or not to be";
+        String a[] = sentence.toLowerCase().split(" ");
+        Map<String, Integer> words = new HashMap<>();
+        for (String word : a) 
+ 	{
+            if (words.containsKey(word)) 
+	     {
+                words.put(word, 1 + words.get(word));
+             } else
+	     {
+                words.put(word, 1);
+             }
+        }
+        System.out.println(words);
+
+    String sentence1 = "Paranoids are not paranoid because they are paranoid but" +
+            "because they keep putting themselves deliberately into paranoid avoidable situations";
+    String b[] = sentence2.toLowerCase().split(" ");
+    Map<String,Integer> words2 = new HashMap<>();
+    for (String word : b)
+       {
+        if (words2.containsKey(word)) 
+        {
+            words2.put(word,1 + words2.get(word));
+        } else 
+        {
+            words2.put(word,1);
+        String sentence2 = "Paranoids are not paranoid because they are paranoid but" +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        String b[] = sentence2.toLowerCase().split(" ");
+        Map<String, Integer> words2 = new HashMap<>();
+        for (String word : b)
+	 {
+            if (words2.containsKey(word)) 
+	     {
+                words2.put(word, 1 + words2.get(word));
+             } else
+	     {
+                words2.put(word, 1);
+             }
+        }
+    }   System.out.println(words2);
+        System.out.println(words2);
+
+        String str = "Paranoids are not paranoid because they are paranoid but" +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
+        String removeWord = "avoidable";
+        remove(str, removeWord);
+    }
+} 
+
